95ff70c852cbe5bc8ba363ebbcf5c57d977b6053
Falon Clark <falon.n.clark@gmail.com>
2026-03-01 21:34:52 -0500

    Add chezmoi autoapply in chezmoi source dir

--- dot_config/nvim/lua/config/autocmd.lua
+++ dot_config/nvim/lua/config/autocmd.lua
@@ -11,3 +11,14 @@ vim.api.nvim_create_autocmd("InsertLeave", {
         vim.opt.relativenumber = false
     end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+    callback = function(ev)
+        local bufnr = ev.buf
+        local edit_watch = function()
+            require("chezmoi.commands.__edit").watch(bufnr)
+        end
+        vim.schedule(edit_watch)
+    end,
+})
